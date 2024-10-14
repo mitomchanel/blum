cf4f95f73a15c401cf053e1d25847158ee5a0126
mitomchanel <anhyvnn@gmail.com>
2024-10-14 13:06:02 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,19 @@
-# blum
+ __  __ _ _                     ____ _                      _ 
+|  \/  (_) |_ ___  _ __ ___    / ___| |__   __ _ _ __   ___| |
+| |\/| | | __/ _ \| '_ ` _ \  | |   | '_ \ / _` | '_ \ / _ \ |
+| |  | | | || (_) | | | | | | | |___| | | | (_| | | | |  __/ |
+|_|  |_|_|\__\___/|_| |_| |_|  \____|_| |_|\__,_|_| |_|\___|_|
+
+Airdrop & Bounty - mitomchanel
+https://t.me/mitomchanel
+
+-Mở cmd tại thư mục
+-Chạy lệnh "pip install -r requirements.txt"
+-Điền API vào file .env
+"API_ID=23554***
+API_HASH=a8f82833e0369a5893b9842351d***"
+API lấy tại: https://my.telegram.org/apps
+-Chạy lệnh "python main.py"
+-Nhập số 1 enter để thêm tài khoản telegram
+Nhập số 2 để chạy bot
+-Ai có sẳn session thì copy vào thư mục sessions là được
---
+++ blum-autoclicker.user.js
@@ -0,0 +1,131 @@
+// ==UserScript==
+// @name         Blum Autoclicker
+// @version      1.2
+// @namespace    Violentmonkey Scripts
+// @author       mitomchanel
+// @match        https://telegram.blum.codes/*
+// @grant        none
+// @homepage     https://t.me/mitomchanel
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    let GAME_SETTINGS = {
+        minBombHits: Math.floor(Math.random() * 2),
+        minIceHits: Math.floor(Math.random() * 2) + 2,
+        flowerSkipPercentage: Math.floor(Math.random() * 11) + 5,
+        dogSkipPercentage: Math.floor(Math.random() * 11) + 2,
+        minDelayMs: 2000,
+        maxDelayMs: 5000,
+        autoClickPlay: true
+    };
+
+    let isGamePaused = false;
+
+    function handleGameElement(element) {
+        if (!element || !element.item) return;
+
+        const { type } = element.item;
+        switch (type) {
+            case "CLOVER":
+                processFlower(element);
+                break;
+            case "BOMB":
+                processBomb(element);
+                break;
+            case "FREEZE":
+                processIce(element);
+                break;
+            case "DOG":
+                processDog(element);
+                break;
+        }
+    }
+
+    function processFlower(element) {
+        const shouldSkip = Math.random() < (GAME_SETTINGS.flowerSkipPercentage / 100);
+        if (!shouldSkip) {
+            clickElement(element);
+        }
+    }
+
+    function processDog(element) {
+        const shouldSkip = Math.random() < (GAME_SETTINGS.dogSkipPercentage / 100);
+        if (!shouldSkip) {
+            clickElement(element);
+        }
+    }
+
+    function processBomb(element) {
+        if (Math.random() < 0.5) {
+            clickElement(element);
+        }
+    }
+
+    function processIce(element) {
+        if (Math.random() < 0.7) {
+            clickElement(element);
+        }
+    }
+
+    function clickElement(element) {
+        if (element && element.onClick && typeof element.onClick === 'function') {
+            element.onClick(element);
+        }
+    }
+
+    function getNewGameDelay() {
+        return Math.floor(Math.random() * (GAME_SETTINGS.maxDelayMs - GAME_SETTINGS.minDelayMs + 1) + GAME_SETTINGS.minDelayMs);
+    }
+
+    function checkAndClickPlayButton() {
+        const playButtons = document.querySelectorAll('button.kit-button.is-large.is-primary, a.play-btn[href="/game"], button.kit-button.is-large.is-primary');
+
+        playButtons.forEach(button => {
+            if (!isGamePaused && GAME_SETTINGS.autoClickPlay && (/Play/.test(button.textContent) || /Continue/.test(button.textContent))) {
+                setTimeout(() => {
+                    button.click();
+                }, getNewGameDelay());
+            }
+        });
+    }
+
+    function continuousPlayButtonCheck() {
+        checkAndClickPlayButton();
+        setTimeout(continuousPlayButtonCheck, 1000);
+    }
+
+    function AutoClaimAndStart() {
+        setInterval(() => {
+            const claimButton = document.querySelector('button.kit-button.is-large.is-drop.is-fill.button.is-done');
+            const startFarmingButton = document.querySelector('button.kit-button.is-large.is-primary.is-fill.button');
+            const continueButton = document.querySelector('button.kit-button.is-large.is-primary.is-fill.btn');
+            if (claimButton) {
+                claimButton.click();
+            } else if (startFarmingButton) {
+                startFarmingButton.click();
+            } else if (continueButton) {
+                continueButton.click();
+            }
+        }, Math.floor(Math.random() * 5000) + 5000);
+    }
+
+    function initializeGame() {
+        const originalPush = Array.prototype.push;
+        Array.prototype.push = function (...items) {
+            if (!isGamePaused) {
+                items.forEach(item => handleGameElement(item));
+            }
+            return originalPush.apply(this, items);
+        };
+
+        continuousPlayButtonCheck();
+        AutoClaimAndStart();
+    }
+
+    // Khởi tạo trò chơi
+    initializeGame();
+
+    // Thêm giao diện cài đặt (có thể thêm vào đây nếu cần)
+})();
